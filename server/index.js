ad6c2f6a3c15164df2400b88365de74b5ae00555
praduin <118813325+praduin@users.noreply.github.com>
2025-09-01 01:28:46 +0530

    this

--- server/index.js
+++ server/index.js
@@ -75,7 +75,12 @@ app.get("/api/data", (req, res) => {
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mydb")
+  // .connect("mongodb://127.0.0.1:27017/mydb")
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
